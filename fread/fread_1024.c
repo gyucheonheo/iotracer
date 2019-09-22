4cb308f60e680594e7027276a6795cffe0520ce7
Gyucheon Heo <gyucheon.heo@gmail.com>
2019-09-22 02:31:38 +0000

    Add missing atoi

--- fread/fread_1024.c
+++ fread/fread_1024.c
@@ -8,7 +8,7 @@ int main(int argc, char**argv){
 	FILE *fp = fopen("test1024.txt", "w+");
 	char *buf = (char *)malloc(sizeof(char)*SZ);
 	fwrite(buf, SZ, 1, fp);
-	sleep(argv[1]));
+	sleep(atoi(argv[1]));
 	fread(buf, SZ, 1, fp);
 	fclose(fp);
 }
